3d6d5b5092f8633e3f4e870550b72856bb89d79d
EC2 Default User <ec2-user@ip-172-31-29-161.us-east-2.compute.internal>
2018-06-04 01:53:24 +0000

    Add a full_titile helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_titile(page_titile = "")
+    base_titile = "Ruby on Rails Tutorial Sample App"
+    if page_titile.empty?
+      base_titile
+    else
+      page_titile + " | " + base_titile
+    end
+  end
 end
