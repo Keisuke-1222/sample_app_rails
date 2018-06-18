403f5954f0d0ad146a6c997ac3665d514bd92374
EC2 Default User <ec2-user@ip-172-31-29-161.us-east-2.compute.internal>
2018-06-18 03:03:05 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,12 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :users
   
   resources :account_activations, only: [:edit]
@@ -26,4 +32,6 @@ Rails.application.routes.draw do
   resources :password_resets, only: [:new, :create, :edit, :update]
   
   resources :microposts, only: [:create, :destroy]
+  
+  resources :relationships, only: [:create, :destroy]
 end
